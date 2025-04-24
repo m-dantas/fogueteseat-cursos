a49400bece5192666bbed7159cc1ab99464e5d38
m-dantas <mauricio.dantascp@gmail.com>
2025-04-24 09:22:12 -0300

    feat(react-03): create a Dialog for new transaction

--- reactjs/dt-money/src/componentes/Header/index.tsx
+++ reactjs/dt-money/src/componentes/Header/index.tsx
@@ -3,14 +3,28 @@ import {
   HeaderContent, 
   NewTransactionButton 
 } from './styles'
-
+import * as Dialog from "@radix-ui/react-dialog"
 import logoImg from '../../assets/logo-ignite.svg'
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>New transaction</NewTransactionButton>
+      
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>New transaction</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
