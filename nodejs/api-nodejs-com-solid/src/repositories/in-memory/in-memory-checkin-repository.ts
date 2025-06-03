0e0d78d66330cf60d8249d152120f3e42b0646b1
m-dantas <mauricio.dantascp@gmail.com>
2025-06-03 14:47:27 -0300

    feat(node-03): add save check-in and findById

--- nodejs/api-nodejs-com-solid/src/repositories/in-memory/in-memory-checkin-repository.ts
+++ nodejs/api-nodejs-com-solid/src/repositories/in-memory/in-memory-checkin-repository.ts
@@ -52,4 +52,23 @@ export class InMemoryCheckInsRepository implements ICheckInsRepository {
     ).length
     return totalCheckInsByUser
   }
+
+  async findById(checkInId: string) {
+    const checkIn = this.items.find((item) => item.id === checkInId)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
+
+    return checkIn
+  }
 }
