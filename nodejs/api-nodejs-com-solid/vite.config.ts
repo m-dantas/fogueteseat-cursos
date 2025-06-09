b420348c2db4a4e84473cae5588d5933350f594d
m-dantas <mauricio.dantascp@gmail.com>
2025-06-09 17:20:06 -0300

    test(node-03): add vitest e2e

--- nodejs/api-nodejs-com-solid/vite.config.ts
+++ nodejs/api-nodejs-com-solid/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
-console.log('✔ Config do Vitest carregada!');
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases'
+        }
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: './prisma/vitest-environment-prisma/prisma-test-environment.ts'
+        }
+      },
+    ]
+  }
 })
