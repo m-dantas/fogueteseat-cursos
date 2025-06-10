6693e459193eaf409f71dd9af578080ff3de9ec5
m-dantas <mauricio.dantascp@gmail.com>
2025-06-10 09:22:13 -0300

    feat(node-03): add verify refresh token in routes

--- nodejs/api-nodejs-com-solid/src/http/controllers/gyms/routes.ts
+++ nodejs/api-nodejs-com-solid/src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { search } from './search.controller'
 import { nearby } from './nearby.controller'
 import { create } from './create.controller'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  app.post('/gyms/create', create)
+  app.post('/gyms/create', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
