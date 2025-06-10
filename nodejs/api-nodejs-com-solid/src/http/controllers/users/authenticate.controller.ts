8df0de0b3cad6b15d4e4acb7c35462e81767202e
m-dantas <mauricio.dantascp@gmail.com>
2025-06-10 09:20:23 -0300

    feat(node-03): add refreshToken on application

--- nodejs/api-nodejs-com-solid/src/http/controllers/users/authenticate.controller.ts
+++ nodejs/api-nodejs-com-solid/src/http/controllers/users/authenticate.controller.ts
@@ -21,10 +21,40 @@ export async function authenticate(
       password,
     })
 
-    const token = await reply.jwtSign({}, { sub: user.id })
-    return reply.status(200).send({
-      token,
-    })
+    const token = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    const refreshToken = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
