f8a5441509a9d7cf102f96515a6dff90231dddd8
Mauricio Dantas <mauricio.dantascp@gmail.com>
2025-08-18 22:15:03 -0300

    feat(node-04): add repository, tests and use-cases for entity Answer

---
+++ nodejs/ddd-no-nodejs/src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question.js'
+import type { AnswersRepository } from '../repositories/answer-repository.js';
+import type { Answer } from '../entities/answer.js';
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+test('create an aswer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
+    content: 'Nova resposta',
+    questionId: '1',
+    instructorId: '1'
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
