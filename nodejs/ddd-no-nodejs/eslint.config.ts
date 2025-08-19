27c1c2e6692201f6b13ada19f440c744895830be
Mauricio Dantas <mauricio.dantascp@gmail.com>
2025-08-19 20:34:01 -0300

    feat(node-04): add prettier lint

--- nodejs/ddd-no-nodejs/eslint.config.ts
+++ nodejs/ddd-no-nodejs/eslint.config.ts
@@ -1,9 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
-]);
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+])
